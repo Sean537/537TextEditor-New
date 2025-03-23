8179fb5b6015a0250a05cfe3af9e0a1c62ddf515
Sean537 <wushaoquan666@outlook.com>
2025-03-23 19:20:29 +0800

    update ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tab->hide();
     initUI();
 }
 
@@ -16,21 +17,22 @@ MainWindow::~MainWindow()
 
 void MainWindow::initUI()
 {
-        setCentralWidget(ui->fileTabWidget); // 将 QTabWidget 设置为中心控件，填充满整个窗口
+    ui->fileTabWidget->removeTab(0);
 
-        // 添加多个标签页
-        for (int i = 0; i < 3; ++i)
-        {
-            // 创建 QPlainTextEdit 控件
-            QPlainTextEdit *textEdit = new QPlainTextEdit();
-            textEdit->setPlaceholderText(QString("Tab %1 - 键入文本...").arg(i + 1));
+    setCentralWidget(ui->fileTabWidget); // 将 QTabWidget 设置为中心控件，填充满整个窗口
 
-            // 将 QPlainTextEdit 控件设置为当前标签页的内容
-            ui->fileTabWidget->addTab(textEdit, QString("Tab %1").arg(i + 1));
-        }
+    // 添加多个标签页
+    for (int i = 0; i < 3; ++i) {
+        // 创建 QPlainTextEdit 控件
+        QPlainTextEdit *textEdit = new QPlainTextEdit();
+        textEdit->setPlaceholderText(QString("Tab %1 - 键入文本...").arg(i + 1));
 
-        // 设置窗口的初始大小
-        resize(800, 600);
+        // 将 QPlainTextEdit 控件设置为当前标签页的内容
+        ui->fileTabWidget->addTab(textEdit, QString("Tab %1").arg(i + 1));
+    }
+
+    // 设置窗口的初始大小
+    resize(640, 480);
 /*
     QLabel *permanent=new QLabel(this);
     permanent->setFrameStyle(QFrame::Box|QFrame::Sunken);
@@ -57,9 +59,24 @@ void MainWindow::on_actionExit_triggered()
 
 void MainWindow::on_actionOpen_triggered()
 {
-    QStringList fileNames = QFileDialog::getOpenFileNames(this, tr("文件对话框"),
-                                                          "", tr("图片文件(*png *jpg)"));
+    /*
+    QStringList fileNames = QFileDialog::getOpenFileNames(this, tr("Open files"),
+                                                          "", tr("Text Files(*txt)"));
     qDebug()<< "fileNames:" << fileNames;
+    */
+    /*
     QString str = fileNames.join("\n"); // 使用换行符连接元素
     QMessageBox::information(this,tr("File"),str);
+    */
+
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open files"),
+                                                          "", tr("Text Files(*txt)"));
+    QString fileContent = QFileDialog::getOpenFileContent(&fileName, nullptr);
+    qDebug()<< "fileName:" << fileName;
+
+    QPlainTextEdit *textEdit = new QPlainTextEdit();
+    textEdit->setPlaceholderText(fileContent);
+
+    // 将 QPlainTextEdit 控件设置为当前标签页的内容
+    ui->fileTabWidget->addTab(textEdit, fileName);
 }
